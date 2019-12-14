0c67867ec56e858e1b831d30d0235ae1ea4cbb71
eshlomo1 <36914463+eshlomo1@users.noreply.github.com>
2019-12-14 12:28:01 +0200

    Update Enable-ASR.ps1

--- Enable-ASR.ps1
+++ Enable-ASR.ps1
@@ -1,11 +1,36 @@
-﻿#Enable ASR for Windows 10 
+﻿# Windows 10 Exploit Guard and Protection Settings
 
-Write-Host "Enabling WD Exploit Guard scenarios in audit mode." -ForegroundColor Green
+## Get Current Settings
 
-# Enable all Attack Surface Reducation rules in audit mode
+### Attack Surface Reducation Rules
+Get-MpPreference | fl AttackSurfaceReductionRules_Ids, AttackSurfaceReductionRules_Actions, AttackSurfaceReductionOnlyExclusions
+
+### Network Protection Current Settings
+Get-MpPreference | fl EnableNetworkProtection 
+
+### Windows Firewall Status
+Get-NetFirewallProfile -All
+Get-NetIPInterface
+
+### ProcessMitigation Current Settings
+Get-ProcessMitigation -FullPolicy
+
+### Controlled Folder Access Settings
+Get-MpPreference | fl EnableControlledFolderAccess
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# Enable Windows 10 Protection
+
+### Enable Network Protection
+Set-MpPreference -EnableNetworkProtection 1
+
+## Enable All Attack Surface Reducation 
+
+### Enable all Attack Surface Reducation rules in audit mode
 Write-Host "Enabling Attack Surface Reducation rules and setting to audit mode"
 
-#Block Office applications from injecting into other processes
+### Block Office applications from injecting into other processes
 Add-MpPreference -AttackSurfaceReductionRules_Ids 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84 -AttackSurfaceReductionRules_Actions Enabled
 
 #Block Office applications from creating executable content
@@ -25,6 +50,3 @@ Add-MpPreference -AttackSurfaceReductionRules_Ids BE9BA2D9-53EA-4CDC-84E5-9B1EEE
 
 #Block Win32 imports from Macro code in Office
 Add-MpPreference -AttackSurfaceReductionRules_Ids 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B -AttackSurfaceReductionRules_Actions Enabled
-
-
-Write-Host "Successfully enabled Windows Defender Exploit Guard scenarios in audit mode." -ForegroundColor Green
