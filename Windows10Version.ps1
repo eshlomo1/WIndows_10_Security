8de546036db3be161686e5ec45996975cd7c3b83
eshlomo1 <36914463+eshlomo1@users.noreply.github.com>
2019-12-14 15:44:02 +0200

    Create Windows10Version.ps1

---
+++ Windows10Version.ps1
@@ -0,0 +1,8 @@
+﻿# Check WIndows 10 Version (used for invokation)
+$winver = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+if ($winver -like "Windows 10*") 
+{Write-Host -ForegroundColor Green Windows 10}
+else
+{Write-Host -ForegroundColor Red Not Windows 10}
+
+#----------------------------------------------------------------------------------------------------------------
